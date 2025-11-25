73ed931967b84063cce68c96f4f62021a9fd36f4
glebofrl <ilya.glebofrl@gmail.com>
2025-11-26 00:28:15 +0300

    Добавлен ввод с клавиатуры.

--- lab5hw/Source.c
+++ lab5hw/Source.c
@@ -7,15 +7,22 @@
 void main()
 {
 	setlocale(LC_CTYPE, "RUS");
-	float x, y, z, fi;
-	float firstCalc, secondCalc;
-	x = -2.235 * pow(10, -2);
-	y = 2.23;
-	z = 15.221;
+	float a, b;
+	int step;
+	printf("¬ведите число в формате \"x*b^n \"   ");
+	scanf("%f*%f^%d", &a, &b, &step);
+	float x, y, z;
+	long double firstCalc, secondCalc, fi;
+	x = a * pow(b, step);
+	printf("¬ведите y: ");
+	scanf("%f", &y);
+	printf("¬ведите z: ");
+	scanf("%f", &z);
+
 	firstCalc = (exp(fabs(x - y)) * pow(fabs(x - y), x + y)) / (atan(x) + atan(z));
 	secondCalc = pow(pow(x, 2) + pow(log(y), 2), 1. / 3);
 	fi = firstCalc + secondCalc;
-	printf("–езультат вычислений: %.3f\n", fi);
+	printf("–езультат вычислений: %.3lf\n", fi);
 
 	system("pause");
 
